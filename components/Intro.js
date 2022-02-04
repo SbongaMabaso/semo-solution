2bdfb3a2a726d7fdcb2f340999070d311e77fe9d
Sbonga Mabaso <sbongamabaso5@gmail.com>
2022-02-04 11:04:15 -0800

    initial commit

---
+++ components/Intro.js
@@ -0,0 +1,40 @@
+import style from "../styles/Intro.module.css";
+import Image from "next/image";
+import Circle from "./Circle";
+
+const Intro = () => {
+  return (
+    <div className={style.container}>
+      {/* <div className={`${style.circle} ${style.circle1}`} />
+      <div className={`${style.circle} ${style.circle2}`} /> */}
+      <Circle backgroundColor="#01c686" top="-45vh" left="-45vh" />
+      {/* <Circle backgroundColor="#01c686" right="-40vh" /> */}
+      <div className={style.card}>
+        <h1 className={style.title}>
+          <span className={style.brandName}>| SEM0 S0LUTI0N |</span>
+          <br />
+           <h6>we are your solution of
+             your digital product.
+            </h6>
+        </h1>
+        <p className={style.desc}>
+          We are a family of young, and energetic software engineers who
+          live and breath coding. Looking for digital product?
+           - w e - g o t - y o u ! go to our contact page
+          and talk to us today. You are exact where you need to be.
+        </p>
+        <button className={style.button}>Read More</button>
+      </div>
+      <div className={style.card}>
+        <Image
+          src={process.env.NEXT_PUBLIC_URL + "/img/semo-logo.png"}
+          layout="fill"
+          objectFit="cover"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
---
+++ components/Navbar.js
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import style from "../styles/Navbar.module.css";
+import Link from "next/link";
+
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className={style.container}>
+      <Link href="/">S e m o | S o l u t i o n</Link>
+      <ul className={style.list}>
+        <li className={style.listItem}>
+        {/* /products/design  - should replace a (/) in a link*/}
+          <Link href="/">DESIGN</Link>
+        </li>
+        <li className={style.listItem}>
+        {/* /products/development */}
+          <Link href="/">DEVELOPMENT</Link>
+        </li>
+        <li className={style.listItem}>
+          {/* /products/production */}
+          <Link href="/">PRODUCTION</Link>
+        </li>
+        <li className={style.listItem}>
+          {/* /products/photography */}
+          <Link href="/">PHOTOGRAPHY</Link>
+        </li>
+        <li className={style.listItem}>
+          <Link href="/contact">CONTACT</Link>
+        </li>
+      </ul>
+      <div className={style.hamburger} onClick={() => setOpen(!open)}>
+        <div className={style.line} />
+        <div className={style.line} />
+        <div className={style.line} />
+      </div>
+      <ul onClick={()=>setOpen(false)} className={style.menu} style={{ right: open ? "0px" : "-50vw" }}>
+        <li className={style.menuItem}>
+          <Link href="/">HOME</Link>
+        </li>
+        <li className={style.menuItem}>
+          <Link href="/products/design">DESIGN</Link>
+        </li>
+        <li className={style.menuItem}>
+          <Link href="/products/development">DEVELOPMENT</Link>
+        </li>
+        <li className={style.menuItem}>
+          <Link href="/products/production">PRODUCTION</Link>
+        </li>
+        <li className={style.menuItem}>
+          <Link href="/products/photography">PHOTOGRAPHY</Link>
+        </li>
+        <li className={style.menuItem}>
+          <Link href="/contact">CONTACT</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
